8668b06bce557cd54dd7a0c5493ae1074a710548
Ville Peurala <ville.peurala@reaktor.fi>
2015-04-02 13:42:10 +0300

    Add Mithril

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var ajankohtaista = {};
+var palvelukuvaus = {};
+var referenssit   = {};
+
+m.route.mode = "hash";
+
+var routes = [
+  { url: "/", title: "Ajankohtaista", module: ajankohtaista },
+  { url: "/palvelukuvaus", title: "Palvelukuvaus", module: palvelukuvaus },
+  { url: "/referenssit", title: "Referenssit", module: referenssit }
+]
+
+m.route(document.getElementById("navigation"), "/", {
+	"/": ajankohtaista,
+	"/palvelukuvaus": palvelukuvaus,
+	"/referenssit": referenssit
+});
+
+m.render(document.getElementById("navigation"), routes.map(function (route) {
+	return m("li", [
+		m("a", { href: route.url, config: m.route }, route.title)
+	])
+}));
 
